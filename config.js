84cb06b0c8ccdea05edbeeea3b95bd0282dfc62a
lyniss <106950695+lyniss@users.noreply.github.com>
2022-06-06 23:18:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'Oppa à!',
+    introDesc: `Trên đời có người mạnh, kẻ yếu 
+    Còn anh và em thì là ngiu 😘`,
+    btnIntro: 'mãi iuuu',
+    title: 'Bỏ ngoài tai chăm lời tán tỉnh 😙',
+    desc: 'Vì tôi chỉ thấy mình anh đẹp trai🥰 ',
+    btnYes: 'Oki, bé iu <33',
+    btnNo: 'Không phải!! :3',
+    question:'Liệu em đã thành công đánh cắp trái tim anh???',
+    btnReply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Trái tim anh đã thuộc về em 😘 ',
+    mess: 'Em biết mà 🥰. Thương nhiều nhiều 😘😘',
+    messDesc: 'Chúc mừng anh đã thuộc quyền sở hữu của em.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
