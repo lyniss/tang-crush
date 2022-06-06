2547e4c9a1092a05d540b80ea292f5fceba0f3a8
lyniss <106950695+lyniss@users.noreply.github.com>
2022-06-06 14:08:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
     introTitle: 'Oppa à!',
-    introDesc: `Trên đời có người mạnh, kẻ yếu
-    Còn anh với em thì là ngiu 😘`,
+    introDesc: `Trên đời có người mạnh, kẻ yếu.
+                Còn anh với em thì là ngiu 😘`,
     btnIntro: 'mãi iu',
     title: 'Bỏ ngoài tai trăm lời tán tỉnh 😙',
     desc: 'Vì em chỉ thấy một mình anh đẹp trai <3',
     btnYes: 'Oki,bé iu <33',
     btnNo: 'Không ạ!!! :3',
     question:'Liệu e đã thành công đánh cắp trái tinh anh? <3',
-    btnReply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    btnReply: 'Trái tim anh đã thuộc về em <3',
     reply: 'Trái tim anh đã thuộc về em <3',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    mess: 'Em biết mà 🥰. Thương anh nhiều nhiều 😘😘',
     messDesc: 'Chúc mừng anh đã thuộc quyền sở hữu của em.',
     btnAccept: 'iu em <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
